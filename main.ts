ab45f70866c2c4ee8da3c8b3d2c29431429605ab
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-22 17:27:27 +0300

    AB basılınca forma

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.TShirt)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
