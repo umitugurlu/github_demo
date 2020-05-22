d7d4ac2e30eae9c93df8f148169ebac502a9e2cf
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-22 17:35:39 +0300

    Butonların yerleri değişti
    Butonların yerleri değişti.  A -->sad oldu B-->happy oldu

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Happy)
 })
