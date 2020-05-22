3a378ab7e0957e17d32651fdd1971609649308b1
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-22 17:28:24 +0300

    Restore '22.05.2020 17:26:03 AB butonuna basılınca kalp'

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.TShirt)
+    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
